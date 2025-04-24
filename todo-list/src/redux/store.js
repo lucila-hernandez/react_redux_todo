cc93b944f2362386be1403c47b503a4a0ef8539b
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-04-23 22:24:39 -0700

    added state persistence

--- todo-list/src/redux/store.js
+++ todo-list/src/redux/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
+import { loadState, saveState } from "./localStorage";
+
+const preloadedState = loadState() || { todos: [] }; 
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+  preloadedState, 
+});
+
+store.subscribe(() => {
+  saveState(store.getState().todos); // Still saving only the todos array
 });
